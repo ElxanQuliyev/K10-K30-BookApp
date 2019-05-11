2bfa7b108aeb388b3ebb851bd1c6d03ab01c2a57
ElxanQuliyev <qelxan177@gmail.com>
2019-05-11 18:30:49 +0400

    Book App

---
+++ BookApp1/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace BookApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBookName = new System.Windows.Forms.TextBox();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbAuthor = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAddBook = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgView
+            // 
+            this.dtgView.AllowUserToAddRows = false;
+            this.dtgView.AllowUserToDeleteRows = false;
+            this.dtgView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgView.Location = new System.Drawing.Point(575, 107);
+            this.dtgView.Name = "dtgView";
+            this.dtgView.ReadOnly = true;
+            this.dtgView.Size = new System.Drawing.Size(683, 675);
+            this.dtgView.TabIndex = 0;
+            this.dtgView.RowHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.DtgView_RowHeaderMouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Book Name";
+            // 
+            // txtBookName
+            // 
+            this.txtBookName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.txtBookName.Location = new System.Drawing.Point(32, 116);
+            this.txtBookName.Name = "txtBookName";
+            this.txtBookName.Size = new System.Drawing.Size(142, 23);
+            this.txtBookName.TabIndex = 2;
+            // 
+            // txtCount
+            // 
+            this.txtCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.txtCount.Location = new System.Drawing.Point(32, 235);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(142, 23);
+            this.txtCount.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Count";
+            // 
+            // cmbAuthor
+            // 
+            this.cmbAuthor.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbAuthor.FormattingEnabled = true;
+            this.cmbAuthor.Location = new System.Drawing.Point(32, 346);
+            this.cmbAuthor.Name = "cmbAuthor";
+            this.cmbAuthor.Size = new System.Drawing.Size(164, 28);
+            this.cmbAuthor.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(28, 308);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Author";
+            // 
+            // btnAddBook
+            // 
+            this.btnAddBook.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddBook.Location = new System.Drawing.Point(32, 423);
+            this.btnAddBook.Name = "btnAddBook";
+            this.btnAddBook.Size = new System.Drawing.Size(142, 51);
+            this.btnAddBook.TabIndex = 7;
+            this.btnAddBook.Text = "Add Book";
+            this.btnAddBook.UseVisualStyleBackColor = true;
+            this.btnAddBook.Click += new System.EventHandler(this.BtnAddBook_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(32, 423);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(142, 51);
+            this.btnEdit.TabIndex = 8;
+            this.btnEdit.Text = "Edit Book";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(32, 497);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(142, 51);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete Book";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1199, 811);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAddBook);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbAuthor);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBookName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtgView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBookName;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbAuthor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAddBook;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ BookApp1/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BookApp1.Model;
+
+namespace BookApp1
+{
+    public partial class Form1 : Form
+    {
+        NewBookEntities db = new NewBookEntities();
+        Book selectedBook;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AddBookBtn();
+            cmbAuthor.Items.AddRange(db.Authors.Select(a => a.FullName).ToArray());
+            LoadBook();
+        }
+        private void LoadBook()
+        {
+            dtgView.DataSource = db.Books.Select(a =>
+                new
+                {
+                    id=a.Id,
+                    Kitab_Adi = a.Name,
+                    Sayi = a.Count,
+                    Muellif = a.Author.FullName,
+                  
+                }
+                ).ToList();
+
+        }
+
+     
+        private void CheckText()
+        {
+            foreach(Control cb in this.Controls)
+            {
+                if(cb is TextBox || cb is ComboBox )
+                {
+                    cb.Text = "";
+                }
+            }
+        }
+        private void BtnAddBook_Click(object sender, EventArgs e)
+        {
+            string bookName = txtBookName.Text;
+            string bookCount = txtCount.Text;
+            string author = cmbAuthor.Text;
+            int bookCounts;
+            if (bookName != String.Empty &&
+                bookCount != String.Empty &&
+                author != String.Empty &&
+                int.TryParse(bookCount, out bookCounts)
+                )
+            {
+                int authorId = db.Authors.First(a => a.FullName == author).Id;
+                Book book = new Book();
+                book.Name = bookName;
+                book.Count = bookCounts;
+                book.author_id = authorId;
+                db.Books.Add(book);
+                db.SaveChanges();
+                CheckText();
+                LoadBook();
+                MessageBox.Show("Kitab ugurla əlavə olundu", "Success",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+            }
+            else
+            {
+                MessageBox.Show("Zehmet olmasa butun xanlari doldurun","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+        private void AddBookBtn()
+        {
+            btnAddBook.Visible = true;
+            btnDelete.Visible = false;
+            btnEdit.Visible = false;
+        }
+        private void DeleteEditBtn()
+        {
+            btnAddBook.Visible = false;
+            btnDelete.Visible = true;
+            btnEdit.Visible = true;
+        }
+
+        private void DtgView_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+           int bookId= (int)dtgView.Rows[e.RowIndex].Cells[0].Value;
+            selectedBook = db.Books.Find(bookId);
+            txtBookName.Text = selectedBook.Name;
+            txtCount.Text = selectedBook.Count.ToString();
+            cmbAuthor.Text = selectedBook.Author.FullName;
+            DeleteEditBtn();
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (selectedBook != null)
+            {
+
+               DialogResult result= MessageBox.Show("Qaqa Silim?", "Information", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                if (result == DialogResult.OK)
+
+                    MessageBox.Show("Qaqa Sildim", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                db.Books.Remove(selectedBook);
+
+                    db.SaveChanges();
+
+                    CheckText();
+                    LoadBook();
+                    AddBookBtn();
+                }
+               
+               
+
+            }
+        }
+}
